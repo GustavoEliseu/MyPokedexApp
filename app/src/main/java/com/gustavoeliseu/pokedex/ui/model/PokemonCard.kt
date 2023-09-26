2fa07e04a333e78fdc8b7fd1cb6cd311b704efc0
Gustavo Amaral Eliseu <30469845+GustavoEliseu@users.noreply.github.com>
2023-09-26 12:05:20 -0300

    Initial commit

---
+++ app/src/main/java/com/gustavoeliseu/pokedex/ui/model/PokemonCard.kt
@@ -0,0 +1,18 @@
+package com.gustavoeliseu.pokedex.ui.model
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.gustavoeliseu.pokedex.R
+
+@Composable
+fun PokemonCard (
+    id:Int,
+    name:String,
+    picture:String
+){
+
+}
